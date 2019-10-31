a01fb75bdc9a9fbefff1bcce0b076d78775dcb1d
Ushiro <kenji.martin.ushiro@accenture.com>
2019-10-31 11:14:22 -0300

    asd

---
+++ Selenium cSharp/Selenium cSharp/TestSelenium/Testeo/Program.cs
@@ -0,0 +1,18 @@
+﻿using Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testeo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebScraper ws = new WebScraper("http://dolarhoy.com/");
+            
+        }
+    }
+}
